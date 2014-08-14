655c37f1acacf21e28ceb6b0e49695bb9beacd93
Ralph Moritz <ralphmoritz@outlook.com>
2014-08-14 09:12:39 +0200

    Remove valdr because it's turning out to be a mission to display validation errors in an aesthetically pleasing way.

--- src/client/app.js
+++ src/client/app.js
@@ -1,8 +1,8 @@
 ﻿'use strict'
 
 angular.module('chessRank', ['ngResource', 'ui.router', 'ncy-angular-breadcrumb',
-    'ui.bootstrap', 'kendo.directives', 'valdr', 'rmUtils.filters'])
-    .config(function ($stateProvider, $urlRouterProvider, $breadcrumbProvider, valdrProvider) {
+    'ui.bootstrap', 'kendo.directives', 'rmUtils.filters'])
+    .config(function ($stateProvider, $urlRouterProvider, $breadcrumbProvider) {
         $breadcrumbProvider.setOptions({
             templateUrl: 'static/views/breadcrumb.html'
         });
@@ -41,92 +41,4 @@ angular.module('chessRank', ['ngResource', 'ui.router', 'ncy-angular-breadcrumb'
                     ncyBreadcrumbLabel: '{{ tournament.name }}'
                 }
             });
-
-        valdrProvider.addConstraints({
-            Login: {
-                email: {
-                    email: {
-                        message: 'Must be a valid email address'
-                    },
-                    required: {
-                        message: 'Email is required'
-                    }
-                },
-                password: {
-                    size: {
-                        min: 8,
-                        max: 200,
-                        message: 'Must be between 8 and 200 characters'
-                    },
-                    required: {
-                        message: 'Password is required'
-                    }
-                },
-                rememberMe: {}
-            },
-            SignUp: {
-                name: {
-                    required: {
-                        message: 'Name is required'
-                    },
-                    size: {
-                        min: 2,
-                        max: 50,
-                        message: 'Must be between 2 and 50 characters'
-                    }
-                },
-                surname: {
-                    required: {
-                        message: 'Surname is required'
-                    },
-                    size: {
-                        min: 2,
-                        max: 50,
-                        message: 'Must be between 2 and 50 characters'
-                    }
-                },
-                dateOfBirth: {
-                    required: {
-                        message: 'Date of Birth is required'
-                    }
-                },
-                email: {
-                    email: {
-                        message: 'Must be a valid email address'
-                    },
-                    required: {
-                        message: 'Email is required'
-                    }
-                },
-                contactNumber: {
-                    required: {
-                        message: 'Contact Number is required'
-                    },
-                    pattern: {
-                        value: /^\+\d{11}$/,
-                        message: 'Must be a valid phone number in the format: +12345678901'
-                    }
-                },
-                fideRating: {
-                    digits: {
-                        integer: 4,
-                        fraction: 0,
-                        message: 'Must be a natural number with less than 5 digits'
-                    }
-                },
-                fedRating: {
-                    digits: {
-                        integer: 3,
-                        fraction: 0,
-                        message: 'Must be a natural number with less than 5 digits'
-                    }
-                },
-                federation: {},
-                union: {},
-                fideTitle: {},
-                fedTitle: {},
-                male: {},
-                female: {}
-            }
-        });
     });
